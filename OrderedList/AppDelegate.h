bd306a3e756361a5978c96f956faa8ac7ed0729d
Ronald Hernandez <2ronald.hernandez@gmail.com>
2015-10-19 17:46:37 -0700

    Initial Commit

---
+++ OrderedList/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  OrderedList
+//
+//  Created by Ronald Hernandez on 10/19/15.
+//  Copyright © 2015 Evernote. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
