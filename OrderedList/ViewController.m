d8826a14411ef28a87b0feb58fb1d7583969f164
Ronald Hernandez <2ronald.hernandez@gmail.com>
2015-10-20 16:10:32 -0700

    able to get number of words based on count

--- OrderedList/ViewController.m
+++ OrderedList/ViewController.m
@@ -9,8 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-//This dictionary holds the words and the requencies of the words.
-@property NSMutableDictionary *wordsDictionary;
+
 @property NSString *text;
 @end
 
@@ -24,20 +23,21 @@
  4. Need to convert all words to lower case
  5. Need to print words in order based on frequency until maxCount (size of the array returned)
  6. We only need to print each word once (no repeated words)
- 
  */
 
+
+/* In the viewDidLoad, we will initialize the text which represents our "file". We will initialize our words dictionary and we will call our method which we will provide two paramenters. 1) text String and 2. maxCount
+*/
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 
-    self.text = @"Katniss Everdeen, who tells her story in the first person, wakes up. It is the day of the reaping. She sees her little sister, Prim (short for Primrose), asleep in bed with their mother across the room. Katniss puts on her clothes to go hunting. The area where she and her family live is called the Seam, and it’s part of District 12. They are at the edge of the district, which is enclosed by a high fence, and Katniss often crawls under the fence and enters the woods outside, where she forages and hunts. Her father taught her these skills before his death in a mine explosion when she was eleven years old, and she uses a bow he made. Though trespassing in the woods and poaching are illegal, nobody pays attention, and Katniss even sells meat to the Peacekeepers who are supposed to enforce the laws. Most people in the district, she explains, don’t have enough food.";
+     self.text = @"Katniss Everdeen, who tells her story in the first person, wakes up. It is the day of the reaping. She sees her little sister, Prim (short for Primrose), asleep in bed with their mother across the room. Katniss puts on her clothes to go hunting. The area where she and her family live is called the Seam, and it’s part of District 12. They are at the edge of the district, which is enclosed by a high fence, and Katniss often crawls under the fence and enters the woods outside, where she forages and hunts. Her father taught her these skills before his death in a mine explosion when she was eleven years old, and she uses a bow he made. Though trespassing in the woods and poaching are illegal, nobody pays attention, and Katniss even sells meat to the Peacekeepers who are supposed to enforce the laws. Most people in the district, she explains, don’t have enough food.";
 
 
-    self.wordsDictionary = [NSMutableDictionary new];
 
-  [self mostFrequentWordsFromString:self.text maxCount:2];
+     NSLog(@"%@", [self mostFrequentWordsFromString:self.text maxCount:9]);
 
 
 
@@ -49,57 +49,59 @@
 }
 
 
-//Function takes two paramenters:
-//1. a String representing a text document and 2) an Integer providing the number of items to return.
+/* Function takes two paramenters:
+ 1. a String representing a text document
+ 2) an Integer providing the number of items to return.
 
-//The function returns a list of Strings ordered by word frequency, the most frequent occurring word first. The solutions should run in O(n) where n is the number of of words in the document.
+ Result: The function returns a list of Strings ordered by word frequency, the most frequent occurring word first. The solutions should run in O(n) where n is the number of of words in the document.
 
-/* 1. check if the "file" is empty. 
- 2. We need to ignore case sensitive
- 3. Remove puntuation or ignore it. 
- 4. Convert string to NSDictonary and keep count of the their frequency. 
-    a) If the word exist, simply update the frequncy of the word for that dictionary. 
+ Plan of Attack
+ 1. check if the "file" is empty.
+ 2. Convert all words to lowercase
+ 3. Remove puntuation
+ 4. Convert the resulting string to an array of strings.
+ 4. Convert string to NSDictonary and keep count of the their frequency.
+    a) If the word exist, simply update the frequncy of the word for that particular key.
     b) if the word does not exists, you need add it to the dictionary and increment the requncy by 1. 
  
 */
 
+
+/* Analysis
+ 
+ */
 -(NSArray *)mostFrequentWordsFromString:(NSString *)text maxCount:(NSUInteger )maxCount{
 
-    NSArray *sortedListCopy = [NSArray new];
+    NSArray *sortedList = [NSArray new];
 
-    //check if the text is empty if, provide a message.
+    //check if the text is empty..if so.. provide a message.
     if ([text isEqualToString:@""]) {
 
-        NSLog(@"the text was empty");
+        NSLog(@"the text file was empty");
 
     }else{
 
-        //remove puntuation. Also need to convert the string received to lower case so all words can be evaluated equally.
-
-
-
-    //Convert the string to an array.
+    //First we need to remove puntuation. Also need to convert the words to lower case so all words can be evaluated equally.
+    //Additionally, we need to sore the words into an array.
 
         NSArray *wordsArray = [[[[text componentsSeparatedByCharactersInSet:[[NSCharacterSet letterCharacterSet]invertedSet]]
                                  componentsJoinedByString:@" "] lowercaseString] componentsSeparatedByString:@" "];
         NSMutableDictionary *wordsDictionary = [NSMutableDictionary new];
-
-
 
     //iterate through the array of words
         for (NSString *str in wordsArray){
 
     //if the word does not exists in the dictionary, we need to add it to the dictionary and set it's initial value to @1;
 
-            if (![wordsDictionary objectForKey:str] && ![str isEqualToString:@""] && wordsDictionary.count != maxCount) {
+            if (![wordsDictionary objectForKey:str] && ![str isEqualToString:@" "]) {
 
                 [wordsDictionary setObject:@1 forKey:str];
 
             }else{
-                //get neeed to get he value for that key
+                //get the value for that key
                 int value = (int)[[wordsDictionary objectForKey:str] integerValue] + 1;
 
-                //then overide the current value wiht the new value.
+                //then overide the current value withthe new value.
 
                 [wordsDictionary setObject:[NSNumber numberWithInt:value] forKey:str];
 
@@ -107,20 +109,21 @@
             }
         }
 
+        //remove any empty strings
         [wordsDictionary removeObjectForKey:@""];
 
-        NSLog(@"%@", wordsDictionary);
+       //We want to sort the list of words based on their values (frequencies). Additionally we want to reverse the order so highest frequency is first.
 
-      NSArray *sortedList = [[[wordsDictionary keysSortedByValueUsingComparator:^NSComparisonResult(id obj1, id obj2) {
+       sortedList = [[[wordsDictionary keysSortedByValueUsingComparator:^NSComparisonResult(id obj1, id obj2) {
 
             return [obj1 compare:obj2];
         }] reverseObjectEnumerator] allObjects];
-
-        NSLog(@"%@", sortedList);
-
     }
 
+    //We only want to return the number of items based on the maxCount. 
 
-    return sortedListCopy;
+    return [sortedList subarrayWithRange:NSMakeRange(0, maxCount-1)];
 }
+
+
 @end
